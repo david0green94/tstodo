b1840f9fd045e314f04e814bc0e717abe2f1c4be
David Green <david0green94@gmail.com>
2022-07-20 17:44:19 +0100

    toggle item working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TodoListItem } from './TodoListItem';
+
+const initialTodos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
 
 
+function App() {
+  const [todos, setTodos] = useState(initialTodos); // returns the current stae and a function to set the state. 
 
-function App(){
-return <>Hello World</>;
+  const toggleTodo = (selectedTodo: Todo) => { //function toggletodo, takes in a Todo of type Selected Todo??
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
 
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+
+return (
+  <>
+    <ul>
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
+    </ul>
+
+  </>
+);
 }
+
+
+
 
 export default App;
--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 
-interface Todo {
 
-    text: string;
-    complete: boolean;
-}
 
 interface Props {
 
-    Todo: Todo;
+    todo: Todo;
+    toggleTodo: ToggleTodo;
 }
 
-export const ToDoListItem: React.FC<Props> = ({ Todo }) => {
+export const TodoListItem: React.FC<Props> = ({ todo, toggleTodo }) => {
 
     return (
 
         <li>
             <label
-                style={{ textDecoration: Todo.complete ? 'line-through' : undefined }}
+                style={{ textDecoration: todo.complete ? 'line-through' : undefined }}
             >
-                <input type="checkbox" checked={Todo.complete} /> {Todo.text}
+                <input
+                    type="checkbox"
+                    checked={todo.complete}
+                    onClick={() => {
+                        toggleTodo(todo);
+                    }}
+                />{' '}
+
+                {todo.text}
 
             </label>
         </li>
