a7cb8725a75fcc3d0f219198744cee5455a283c5
David Green <david0green94@gmail.com>
2022-07-20 18:53:33 +0100

    new branch to add chakraUI

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -2,14 +2,13 @@ import React from "react";
 
 
 
-interface Props {
-
-    todo: Todo;
-    toggleTodo: ToggleTodo;
+interface Props { 
+    todo: Todo; //(local?)prop todo  is of Type Todo? 
+    toggleTodo: ToggleTodo; // (local?)prop toggleTodo is of type Toggletodo? 
 }
 
 export const TodoListItem: React.FC<Props> = ({ todo, toggleTodo }) => {
-
+// Dont really understand the interface and <Props> part, but it sure looks like C# Types. 
     return (
 
         <li>
